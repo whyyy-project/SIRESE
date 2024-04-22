bbc235fca4f3073603cd95cbb505cb8b0a1c0442
Wahyu Nur Cahyo <116796627+whyyy-project@users.noreply.github.com>
2024-04-22 19:28:18 +0700

    seeder data smartphone

--- app/Database/Seeds/DataSmartphone.php
+++ app/Database/Seeds/DataSmartphone.php
@@ -8,6 +8,29 @@ class DataSmartphone extends Seeder
 {
     public function run()
     {
-        //
+        $data = [
+            [
+                'brand' => 'ASUS',
+                'merek' => 'Merek X',
+                'build' => 'Build 1',
+                'ram' => '4',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'brand' => 'Brand B',
+                'merek' => 'Merek Y',
+                'build' => 'Build 2',
+                'ram' => '8',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        // Load model
+        $produkModel = model('App\Models\ProdukModel');
+
+        // Insert data
+        $produkModel->insertBatch($data);
     }
 }
