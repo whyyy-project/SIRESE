4fc78939d527ab3c3cb56666695ae3185b6bc154
Wahyu Nur Cahyo <116796627+whyyy-project@users.noreply.github.com>
2024-05-04 15:22:17 +0700

    add title widget

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -4,22 +4,22 @@
     <div class="bg-cyan-950 shadow-xl shadow-none h-16 fixed bottom-0 mt-12 md:relative md:h-full z-10 w-full md:w-48">
         <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
             <ul class="list-reset text-cyan-100 flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
-                <li class="mr-3 flex-1" id="option">
+                <li class="mr-3 flex-1" id="option" title="Halaman Beranda">
                     <a href="<?= base_url() ?>" id="<?= $page == "dashboard" ? "active" : "" ?>" class="block py-1 md:py-3 pl-1 align-middle no-underline border-b-2 border-cyan-950">
                         <i class="fas fa-home pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Beranda</span>
                     </a>
                 </li>
-                <li class="mr-3 flex-1" id="option">
+                <li class="mr-3 flex-1" id="option" title="Sistem Rekomendasi Smartphone">
                     <a href="rekomendasi" id="<?= $page == "rekomendasi" ? "active" : "" ?>" class="block py-1 md:py-3 pl-1 align-middle no-underline border-b-2 border-cyan-950">
                         <i class="fa-solid fa-thumbs-up pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Rekomendasi</span>
                     </a>
                 </li> 
-                <li class="mr-3 flex-1" id="option">
+                <li class="mr-3 flex-1" id="option" title="Data Smartphone">
                     <a href="<?= base_url() ?>data-smartphone" id="<?= $page == "smartphone" ? "active" : "" ?>" class="block py-1 md:py-3 pl-1 align-middle no-underline border-b-2 border-cyan-950">
                         <i class="fa-solid fa-mobile pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Smartphone</span>
                     </a>
                 </li>
-                <li class="mr-3 flex-1" id="option">
+                <li class="mr-3 flex-1" id="option" title="Toko kerjasama">
                     <a href="<?= base_url() ?>toko" id="<?= $page == "toko" ? "active" : "" ?>" class="block py-1 md:py-3 pl-1 align-middle no-underline border-b-2 border-cyan-950">
                         <i class="fa-solid fa-shop pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Toko</span>
                     </a>
