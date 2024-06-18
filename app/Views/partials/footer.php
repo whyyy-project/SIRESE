951c8bdc79e6019718fbb7fc813082bb948551bc
Wahyu Nur Cahyo <116796627+whyyy-project@users.noreply.github.com>
2024-06-19 01:31:48 +0700

    fixing

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -17,7 +17,7 @@
 
 <?php if(session()->get('LoggedIn')){ ?>
 <!-- Modal Konfirmasi Logout -->
-<div id="logoutModal" class="fixed inset-0 flex items-center justify-center z-50">
+<div id="logoutModal" class="fixed inset-0 flex items-center justify-center z-50 hidden">
     <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
     <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
